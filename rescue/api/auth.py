fdbd581e17e535b83a2c4dfcc1fb84d262de1ebd
alkenova <gulimalkenovaa@gmail.com>
2019-05-08 10:14:37 +0600

    user model

--- rescue/api/auth.py
+++ rescue/api/auth.py
@@ -1,58 +1,58 @@
-# from rest_framework import generics, status
-# from rest_framework.permissions import AllowAny, IsAuthenticated
-# from rest_framework.response import Response
-# from rest_framework.views import APIView
-# from rest_framework.decorators import api_view
-# from rest_framework.authtoken.serializers import AuthTokenSerializer
-# from rest_framework.authtoken.models import Token
-# from api.models import CustomUser
-# from api.serializers import CustomUserSerializer
-#
-#
-# class UserCreateView(APIView):
-#     permission_classes = (AllowAny,)
-#
-#     def post(self, request):
-#         serializer = CustomUserSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#
-#         user = CustomUser.objects.create(
-#             email=serializer.validated_data['email'],
-#             username=serializer.validated_data['email']
-#         )
-#
-#         user.set_password(serializer.validated_data['password'])
-#         user.save()
-#
-#         return Response(status=status.HTTP_201_CREATED)
-#
-#
-# class UserView(APIView):
-#     permission_classes = (AllowAny,)
-#
-#     def get(self, request):
-#         if bool(request.user.is_anonymous):
-#             return Response()
-#
-#         return Response(CustomUserSerializer(request.user).data)
-#
-#
-# class UserList(generics.ListAPIView):
-#     queryset = CustomUser.objects.all()
-#     serializer_class = CustomUserSerializer
-#     permission_classes = (IsAuthenticated, )
-#
-#
-# @api_view(['POST'])
-# def login(request):
-#     serializer = AuthTokenSerializer(data=request.data)
-#     serializer.is_valid(raise_exception=True)
-#     user = serializer.validated_data.get('user')
-#     token, created = Token.objects.get_or_create(user=user)
-#     return Response({'token': token.key})
-#
-#
-# @api_view(['POST'])
-# def logout(request):
-#     request.auth.delete()
-#     return Response(status=status.HTTP_200_OK)
+from rest_framework import generics, status
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.models import Token
+from api.models import CustomUser
+from api.serializers import CustomUserSerializer
+
+
+class UserCreateView(APIView):
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        serializer = CustomUserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = CustomUser.objects.create(
+            email=serializer.validated_data['email'],
+            username=serializer.validated_data['email']
+        )
+
+        user.set_password(serializer.validated_data['password'])
+        user.save()
+
+        return Response(status=status.HTTP_201_CREATED)
+
+
+class UserView(APIView):
+    permission_classes = (AllowAny,)
+
+    def get(self, request):
+        if bool(request.user.is_anonymous):
+            return Response()
+
+        return Response(CustomUserSerializer(request.user).data)
+
+
+class UserList(generics.ListAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+    permission_classes = (IsAuthenticated, )
+
+
+@api_view(['POST'])
+def login(request):
+    serializer = AuthTokenSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    user = serializer.validated_data.get('user')
+    token, created = Token.objects.get_or_create(user=user)
+    return Response({'token': token.key})
+
+
+@api_view(['POST'])
+def logout(request):
+    request.auth.delete()
+    return Response(status=status.HTTP_200_OK)
--- rescue/api/serializers.py
+++ rescue/api/serializers.py
@@ -1,9 +1,9 @@
-# from rest_framework import serializers
-# from api.models import CustomUser
-#
-#
-# class CustomUserSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = CustomUser
-#         fields = ('first_name', 'last_name', 'email',
-#                   'username', 'birth_date', 'gender', 'address')
+from rest_framework import serializers
+from api.models import CustomUser
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ('first_name', 'last_name', 'email',
+                  'username', 'birth_date', 'gender', 'address')
