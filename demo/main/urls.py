c28d61d4180e5cc548551163cc4c1e44673f4ac1
alkenova <gulimalkenovaa@gmail.com>
2019-05-13 16:11:41 +0600

    removed files

--- demo/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from main import views
-
-urlpatterns = [
-    path('categories/', views.CategoryList.as_view()),
-    path('products/', views.products.as_view()),
-    path('products/<int:pk>/', views.product_detail.as_view()),
-    path('products/<int:pk>/comments', views.comment.as_view()),
-]
--- demo/main/views/__init__.py
+++
@@ -1,1 +0,0 @@
-from .generic_cbv import CategoryList, products, product_detail, comment, comment_detail
